c9315b84449e0cb668324ce535c9494ec155156f
alaindemogit <alainjustengabo@gmail.com>
2021-01-18 15:35:16 +0100

    update question1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # alertelaposte
+
+##description 
+
+ Ce projet correspond à ce [sujet](https://simplonco.github.io/nanterre-p10-devdata/s1/python/projet/projet.html)
+ 
--- alertelaposte.py
+++ alertelaposte.py
@@ -1,14 +1,22 @@
-fichier=open('Input/connexion.log','r'+)
-print ('bonjour') #juste pour vérifier si mon terminal fonctionne
-fichierUser= open('Output/utilisateurs.txt','w')
+fichier=open('Input/connexion.log','r')
+#print ('bonjour') #juste pour vérifier si mon terminal fonctionne
+lignes=fichier.readlines()
+liste_utilisateurs=[]
+fichierUser=open('Output/utilisateurs.txt','w')
+
 for ligne in lignes:
-    user = ligne.split(";")[1]
-    fichierUser.write(user + '\n')
+    user=ligne.split(";")[1]
+    if user not in liste_utilisateurs:
+        liste_utilisateurs.append(user)
+        fichierUser.write(user+'\n')
+    
+
 heuresAutorises = ['08','09','10','11','12','13','14','15','16','17','18']
 for ligne in lignes:
     heure=ligne.split(";")[2].split(" ")[1].split(":")[0]
     if heure not in heuresAutorises:
         print (ligne.split(";")[1]+ ' '+ligne.split(";")[0])
+
 fichier.close()
 fichierUser.close()
    
